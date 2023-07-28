2ad1001e94c75aa423da7095546b18af68c3e837
648540858 <648540858@qq.com>
2023-07-28 17:39:43 +0800

    修复录像并发下载内存占用过多的问题

---
+++ src/main/java/top/panll/assist/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package top.panll.assist.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import top.panll.assist.dto.UserSettings;
+
+import java.io.File;
+
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private UserSettings userSettings;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        File file = new File(userSettings.getRecord());
+        registry.addResourceHandler("/download/**").addResourceLocations("file://" + file.getAbsolutePath() + "/");
+        super.addResourceHandlers(registry);
+    }
+}
